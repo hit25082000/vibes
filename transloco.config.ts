d0921cf39d647c59f334861f1a696a577dbaaae5
Luiz Eduardo Domingues <domingues-pc@hotmail.com>
2025-04-12 10:11:42 -0400

    remove trasnsloco and eslint line cr

--- transloco.config.ts
+++
@@ -1,9 +0,0 @@
-import { TranslocoGlobalConfig } from '@jsverse/transloco-utils';
-
-const config: TranslocoGlobalConfig = {
-  rootTranslationsPath: 'apps/vibes/public/i18n/',
-  langs: ['pt-BR', 'en'],
-  keysManager: {},
-};
-
-export default config;
